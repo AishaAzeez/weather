a5862d35bc96ff3c722b4ef9a34b548465e719f0
unknown <aishaazeez90@gmail.com>
2023-12-05 21:04:21 +0530

    last update

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ async function getData(lat, long) {
     try {
         const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=967b79616f4148bf973180351230312&q=${lat},${long}&aqi=yes`);
         let data = await response.json();
-        data = data.current.temp_c;
+        data = data.location.name;
 
         return data; // Return the temperature data
     } catch (error) {
@@ -16,13 +16,36 @@ async function getData(lat, long) {
 
 async function gotLocation(position) {
     try {
-        let temperature = await getData(position.coords.latitude, position.coords.longitude);
-        first.innerHTML = `<div>
-        <h5 style="color:brown ;">Temperature for your current location--${temperature}°C</h5>
-        </div>
-`
+        let place = await getData(position.coords.latitude, position.coords.longitude);
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=5b4bee0ba241d092159faf007e166080`)
+        .then(response => response.json()).then(out => displayData(out))
+        function displayData(dataArray) {
+          place=dataArray.name
+          tempd = dataArray.main.temp
+          tempd -= 273.15
+          tempd = tempd.toFixed(2)
+          wind=dataArray.wind.speed
+          humid=dataArray.main.humidity
+          first.innerHTML = `<div>
+            <P>${tempd}°C</P>
+            </div>
+    `
+    cit.innerHTML = `<div>
+            <P>${place}</P>
+            </div>
+    `
+    win.innerHTML = `<div>
+            <P>${wind} Km/s</P>
+            </div>
+    `
+    hum.innerHTML = `<div>
+            <P>${humid} %</P>
+            </div>
+    `
   
-} catch (error) {
+  
+  
+        }} catch (error) {
         console.error('Error getting location:', error);
         // Handle the error as needed
     }
@@ -40,4 +63,15 @@ button.addEventListener('click', () => {
 button.addEventListener('touchstart', async () => {
     navigator.geolocation.getCurrentPosition(gotLocation, failedToGet);
 });
+
+
+
+
+
+
+
+
+
+
+
 
